1be1a558860cefb0e94c4494f803a658bd8458c8
Trevis McGill <trevismcgill25@gmail.com>
2020-07-30 14:49:39 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,6 +4,7 @@
 def key_for_min_value(name_hash)
 lowest_value = nil
 selected_key = nil
+
   name_hash.each do |key, value|
     if lowest_value == nil || value < lowest_value
       lowest_value = value
