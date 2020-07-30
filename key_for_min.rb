8e625990290893103fa540572c1963995ba0d949
Trevis McGill <trevismcgill25@gmail.com>
2020-07-30 14:48:14 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,11 +2,11 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-lowest_value = 0
+lowest_value = nil
 lowest_key = nil
   name_hash.each do |key, value|
 
-    if lowest_value == 0 || value < lowest_value
+    if lowest_value == nil || value < lowest_value
       lowest_value = value
       lowest_key = key
     end
